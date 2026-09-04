b081329d51f64eb1835fcbc270f856e58ce861e5
walidoutaleb7-hub <walidoutaleb7@gmail.com>
2026-09-04 21:02:33 +0100

    Update MainActivity.java

--- app/src/main/java/com/footballai/MainActivity.java
+++ app/src/main/java/com/footballai/MainActivity.java
@@ -2,24 +2,48 @@ package com.footballai;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
 
+    private WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        WebView webView = new WebView(this);
+        webView = new WebView(this);
 
+        WebSettings settings = webView.getSettings();
+
+        // JavaScript
+        settings.setJavaScriptEnabled(true);
+
+        // LocalStorage
+        settings.setDomStorageEnabled(true);
+
+        // Internet / Web content
+        settings.setDatabaseEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
+
+        // تحميل الصفحات والروابط داخل WebView
         webView.setWebViewClient(new WebViewClient());
 
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.getSettings().setDomStorageEnabled(true);
-
+        // تحميل اللعبة
         webView.loadUrl("file:///android_asset/index.html");
 
         setContentView(webView);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
