9680cd1aa57f956d75e0f9b2fffb66444bf3c182
walidoutaleb7-hub <walidoutaleb7@gmail.com>
2026-09-04 16:15:28 +0100

    Update MainActivity.java

--- app/src/main/java/com/footballai/MainActivity.java
+++ app/src/main/java/com/footballai/MainActivity.java
@@ -6,16 +6,19 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         WebView webView = new WebView(this);
         webView.setWebViewClient(new WebViewClient());
+
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setAllowFileAccess(true);
 
-        webView.loadUrl("http://10.0.2.2:3000");
+        webView.loadUrl("file:///android_asset/index.html");
 
         setContentView(webView);
     }
