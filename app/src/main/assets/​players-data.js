30666b3b1be5a73ff44482ab01227317a79a9cf2
walidoutaleb7-hub <walidoutaleb7@gmail.com>
2026-09-05 21:22:52 +0100

    Create ​players-data.js

---
+++ app/src/main/assets/​players-data.js
@@ -0,0 +1,39 @@
+const DB_PLAYERS = {
+    easy: [
+        { name: "كريستيانو رونالدو", image: "https://media.api-sports.io/football/players/257.png" },
+        { name: "ليونيل ميسي", image: "https://media.api-sports.io/football/players/154.png" },
+        { name: "كيليان مبابي", image: "https://media.api-sports.io/football/players/278.png" },
+        { name: "إيرلينغ هالاند", image: "https://media.api-sports.io/football/players/1100.png" },
+        { name: "محمد صلاح", image: "https://media.api-sports.io/football/players/306.png" },
+        { name: "كيفن دي بروين", image: "https://media.api-sports.io/football/players/886.png" },
+        { name: "جود بيلينغهام", image: "https://media.api-sports.io/football/players/22828.png" },
+        { name: "نيمار جونيور", image: "https://media.api-sports.io/football/players/212.png" },
+        { name: "فينيسيوس جونيور", image: "https://media.api-sports.io/football/players/39715.png" },
+        { name: "روبرت ليفاندوفسكي", image: "https://media.api-sports.io/football/players/521.png" }
+    ],
+    medium: [
+        { name: "رياض محرز", image: "https://media.api-sports.io/football/players/1712.png" },
+        { name: "بوكايو ساكا", image: "https://media.api-sports.io/football/players/1465.png" },
+        { name: "جمال موسيالا", image: "https://media.api-sports.io/football/players/158223.png" },
+        { name: "فيل فودين", image: "https://media.api-sports.io/football/players/648.png" },
+        { name: "لامين يامال", image: "https://media.api-sports.io/football/players/309971.png" },
+        { name: "أنطوان غريزمان", image: "https://media.api-sports.io/football/players/229.png" },
+        { name: "برونو فرنانديز", image: "https://media.api-sports.io/football/players/905.png" },
+        { name: "سون هيونغ مين", image: "https://media.api-sports.io/football/players/192.png" }
+    ],
+    hard: [
+        { name: "فيدي فالفيردي", image: "https://media.api-sports.io/football/players/22904.png" },
+        { name: "ويليام ساليبا", image: "https://media.api-sports.io/football/players/1458.png" },
+        { name: "ديكلان رايس", image: "https://media.api-sports.io/football/players/18928.png" },
+        { name: "رافائيل لياو", image: "https://media.api-sports.io/football/players/28929.png" },
+        { name: "نيكولو باريلا", image: "https://media.api-sports.io/football/players/139.png" },
+        { name: "مارتن أوديغارد", image: "https://media.api-sports.io/football/players/359.png" }
+    ],
+    legend: [
+        { name: "زين الدين زيدان", image: "https://media.api-sports.io/football/players/114979.png" },
+        { name: "رونالدينيو", image: "https://media.api-sports.io/football/players/115000.png" },
+        { name: "أندريس إنييستا", image: "https://media.api-sports.io/football/players/115010.png" },
+        { name: "كاكا", image: "https://media.api-sports.io/football/players/114995.png" },
+        { name: "رونالدو نازاريو", image: "https://media.api-sports.io/football/players/114990.png" }
+    ]
+};
